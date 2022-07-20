cf5ee2469459820beaa82d8a7ccd5e2c56e0d905
full <zj15355208214@gmail.com>
2022-07-20 22:07:55 +0800

    头部布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, Suspense, ComponentType } from 'react';
+import React, { Fragment, ComponentType } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import RenderRouter from './router';
 import store from './store';
@@ -8,13 +8,10 @@ import WithReduxProvider from './components/HOC/withReduxProvider';
 import withErrorScreen from './components/ErrorScreen/withErrorScreen';
 import withAntdConfig from './components/HOC/withAntdConfig';
 import withOfflineMask from './components/HOC/withOfflineMask';
-import { selectToken } from './store/user/userSlice';
-import { useAppSelector } from '@/store/hooks';
 import { useTheme } from '@/hooks';
 
 const RouteComponent = (props: any) => {
     // props接收前面所有高阶组件传过来的值，按需传给要渲染的子组件
-    const token = useAppSelector(selectToken);
     return (
         <Router>
             <RenderRouter></RenderRouter>
@@ -34,6 +31,7 @@ const renderer: (c: ComponentType) => ComponentType = compose(
 const Main = renderer(RouteComponent);
 
 function App() {
+    //初始化主题色
     const { themeSuccess } = useTheme();
     return themeSuccess ? <Main></Main> : <Fragment></Fragment>;
 }
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,9 @@
 import React, { FC, Suspense } from 'react';
 import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
-import Sider from '@/layouts/components/Sider';
-import Footer from '@/layouts/components/Footer';
-import Header from '@/layouts/components/Header';
+import Sider from './components/Sider';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import Loading from '@/components/Loading';
 
 import './index.scoped.scss';
@@ -16,7 +16,7 @@ const DefaultLayout: FC<DefaultLayoutProps> = (props) => {
     return (
         <Layout>
             <Sider></Sider>
-            <Layout>
+            <Layout className="m-content-layout-box">
                 <Header></Header>
                 <Content className="m-content-box">
                     <Suspense fallback={<Loading></Loading>}>
