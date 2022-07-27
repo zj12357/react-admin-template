64c188c280af1f1cf71c2452bd7271b7ea1258b5
norman <zj1378538318@qq.com>
2022-07-27 16:15:24 +0800

    AddCustomer

---
+++ src/pages/AccountManagement/AccountCustomer/AddCustomer/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+type AddCustomerProps = {};
+
+const AddCustomer: FC<AddCustomerProps> = (props) => {
+    return <div>I am AddCustomer</div>;
+};
+
+export default AddCustomer;
